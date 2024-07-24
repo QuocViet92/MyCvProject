717a51f080ef7cf40c1254393057aad7f71d03e2
quocviet92 <luongquocviet13121992@gmail.com>
2024-07-24 19:21:09 +0700

    FixCss

--- src/Pages/Project/project.jsx
+++ src/Pages/Project/project.jsx
@@ -5,7 +5,9 @@ export default function Project({src ,name ,type,id ,state, ...props}){
     return (<>
        <div className="project-box animition-left" key={id}>
         <Link to={`/project/${id}`} state={state}>
+        <div className="img-project-box">
        <img className="project-box-img" src={src} alt={name}/>
+       </div>
        <h4>Name  :<span> {name}</span></h4>
        <p>Type : <span>{type}</span> </p>
       
